ce6e6e914d44c54607a0d5184986c5690577012b
PronayBormon <pronaykumarbormon@gmail.com>
2023-08-21 19:33:08 +0600

    rady for check

--- js/login_popup.js
+++ js/login_popup.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  $(".toggle-password").click(function() {
+    $(this).toggleClass("fa-eye-slash fa-eye");
+    input = $(this).parent().find("input");
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+});
 
     // login popup 
   $(".login_popup_show").on("click", function(){
